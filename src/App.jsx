382e15010dce01f11cb47f2c9537b6999629f13b
ZSzabolcs <zelenaksz@kkszki.hu>
2026-02-24 08:28:42 +0100

    IDEMITKELÍRNI

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,75 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import schoolLogo from './assets/kando.jpg'
+import axios from 'axios';
 import './App.css'
 
 export const App = () => {
-  const [count, setCount] = useState(0)
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [token, setToken] = useState('');
+  const [data, setData] = useState('');
+  const [logged, setLogged] = useState(false);
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post('https://jwt.sulla.hu/login', {
+        username,
+        password,
+      });
+      setToken(response.data.token);
+      console.log("Token: ", response.data.token);
+      setLogged(true);
+    } catch (error) {
+      console.error("Hitelesítés sikertelen: ",error);
+    }
+  };
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://jwt.sulla.hu/termekek', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setData(response.data);
+      console.log("Végpont adatok: ", response.data);
+    } catch (error) {
+      console.error("Végpont lekérdezés sikertelen: ",error);
+    
+  }};
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://www.kkszki.hu" target="_blank"><img src={schoolLogo} alt="Iskola logó" width="200" /></a>
+      <div style={{padding: '28px' }}>
+          {token ? (
+        <div>
+          <h1>Be van jelentkezve - Üdvözöljük!</h1>
+          <div style={{ marginTop: '28px', border: '1px solid #ccc', padding: '15px'}}>
+          <h2>A védett végpont adatai</h2>
+          <button onClick={fetchData}>Végpont lekérdezés</button>
+          {data && (
+            <ul>
+              {data.map((item) => (
+                <li key={item.id}> {item.id} - {item.name} - {item.price} </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        <br />
+        <button onClick={() =>{ setToken(''); setData(null);}}>Kijelentkezés</button>
+        </div>
+      ) : (
+         <div><h1>Bejelentkezés</h1>
+        Felhasználó: <input type="text"
+        placeholder='felhasználónév'
+        value={username}
+        onChange={(event) => setUsername(event.target.value) } /> <br />
+        Jelszó: <input type="password"
+        placeholder='jelszó'
+        value={password}
+      onChange={(event) => setPassword(event.target.value) } /> <br />
+      <button onClick={handleLogin}>Bejelentkezés</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          a számláló állása: {count}
-        </button>
-        <p>
-          Módosítsd az <code>src/App.jsx</code>-t és mentsd le, hogy tesztelhesd a HMR-t!
-        </p>
+        )
+        }
       </div>
-      <p className="read-the-docs">
-         Kattints a React, a Kandó, vagy a Vite logókra, további információkért!
-      </p>
-    </>
-  )
-}
-
-export default App
+  );
+};
